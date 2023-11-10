2142f2d8641f1c2ba192a37914b87586d0fba706
Kelvin Ziro <kelvin.ziroh@gmail.com>
2023-11-10 14:50:59 +0300

    Add single line comments in the sample C source code

--- sample.c
+++ sample.c
@@ -6,8 +6,10 @@ int main() {
     int sum = a + b;
 
     if (sum > 15) {
+        // Print the following string if sum is greater than 15
         printf("Sum is greater than 15\n");
     } else {
+        // Print the followinng string if sum is less than or equal to 15
         printf("Sum is not greater than 15\n");
     }
 
