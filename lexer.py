1528955d49bcd160c4f862e205da0d55ff26e490
Kelvin Ziro <kelvin.ziroh@gmail.com>
2023-11-10 13:24:23 +0300

    Implement a tokenize method in the Lexer class

--- lexer.py
+++ lexer.py
@@ -16,3 +16,28 @@ class Lexer:
         self.source_code = source_code
         self.position = 0
         self.tokens = []
+    
+    def tokenize(self):
+        while self.position < len(self.source_code):
+            char = self.source_code[self.position]
+
+            # Skip whitespace
+            if char.isspace():
+                self.position += 1
+                continue
+
+            # Check for keywords, identifiers and literals
+            if char.isalpha():
+                token = self.tokenize_identifier_or_keyword()
+            elif char.isdigit():
+                token = self.tokenize_literal()
+            else:
+                token = self.tokenize_operator()
+            
+            if token:
+                self.tokens.append(token)
+            else:
+                print(f"Error: Invalid character '{char}' at position {self.position}")
+                self.position += 1
+
+        return self.tokens
